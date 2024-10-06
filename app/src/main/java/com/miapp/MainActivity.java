9d43bcd8e55a0214e7ad8ead30004955fae9e4a4
Spectro64 <153745384+Spectro-64@users.noreply.github.com>
2024-10-06 14:53:37 -0300

    Add files via upload

--- app/src/main/java/com/miapp/MainActivity.java
+++ app/src/main/java/com/miapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.miapp;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Iniciar App Center SDK
+        AppCenter.start(getApplication(), "f85d2617-75dd-4f23-b22f-4a52d6dea89b", 
+                        Analytics.class, Crashes.class);
     }
 }
